9a8133e4307b346545bbdb24b912521f1c4b2af2
Øyvind Lundesgaard <oyvind.lundesgaard@npolar.no>
2023-02-17 21:47:29 +0100

    Interpolation function for velocity. Possibility of requiring a certain fraction of good "uocean" for calculating "Uocean". Some other minor work in *sig_vel.py*.

--- sigpyproc/sig_vel.py
+++ sigpyproc/sig_vel.py
@@ -6,7 +6,7 @@ Functions for processing ocean and sea ice drift velocity.
 
 import numpy as np
 import xarray as xr
-
+from scipy.interpolate import interp1d
 
 def calculate_ice_vel(DX, avg_method = 'median'):
     '''
@@ -100,7 +100,10 @@ def calculate_ocean_vel(DX, avg_method = 'median'):
 
 
 
-def _calculate_uvocean_avg(DX, avg_method = 'median'):
+def _calculate_uvocean_avg(DX, avg_method = 'median', min_good_pct = False):
+    '''
+    TBD Document! (min_good_pct)
+    '''
 
     if avg_method=='median':
         DX['Uocean'] = DX.uocean.median(dim = 'SAMPLE')
@@ -112,12 +115,29 @@ def _calculate_uvocean_avg(DX, avg_method = 'median'):
         raise Exception('Invalid "avg_method" ("%s"). '%avg_method
          + 'Must be "mean" or "median".')
 
+    if min_good_pct:
+        N_before = np.sum(~np.isnan(DX.Uocean))
+        good_ind = np.isnan(DX.uocean).mean(dim='SAMPLE')<1-min_good_pct/100
+        N_after = np.sum(good_ind)
+        min_good_str = (
+            '\nRejected %i of %i ensembles (%.2f%%) with <%.1f%% good samples.'%(
+                N_before-N_after, N_before, (N_before-N_after)/N_before*100,
+                min_good_pct))
+        DX['Uocean'] = DX.Uocean.where(good_ind)
+        DX['Vocean'] = DX.Vocean.where(good_ind)
+    
+    else:
+        min_good_str = ''
+
+
     DX.Uocean.attrs = {'units':'m s-1', 'long_name':'Eastward ocean velocity', 
         'details': 'Ensemble average (%s)'%avg_method, 
-        'processing_history':DX.uocean.processing_history} 
+        'processing_history':DX.uocean.processing_history + min_good_str} 
     DX.Vocean.attrs = {'units':'m s-1', 'long_name':'Northward ocean velocity', 
         'details': 'Ensemble average (%s)'%avg_method, 
-        'processing_history':DX.vocean.processing_history} 
+        'processing_history':DX.vocean.processing_history + min_good_str} 
+
+
 
     return DX
 
@@ -150,10 +170,7 @@ def _calculate_bin_depths(DX):
 
 
 
-def _uvoc_mask_sidelobe(DX):
-    pass
-
-def _uvoc_mask_range(DX, uv_max = 1.5, tilt_max = 5, 
+def uvoc_mask_range(DX, uv_max = 1.5, tilt_max = 5, 
         sspd_range = (1400, 1560), cor_min = 60,
         amp_range = (30, 85), max_amp_increase = 20):
     '''
@@ -254,6 +271,9 @@ def _uvoc_mask_range(DX, uv_max = 1.5, tilt_max = 5,
         DX[key] = DX_uv[key] 
         DX[key].attrs['processing_history'] += proc_string
 
+    # Recompute sample averages
+    DX = _calculate_uvocean_avg(DX)
+
     return DX
 
 
@@ -323,3 +343,149 @@ def rotate_vels_magdec(DX):
 
     return DX
 
+
+def clear_empty_bins(DX, thr_perc=5):
+    '''
+    Remove BINS where less than thr_perc of the bin contains valid
+    (non-NaN) samples.
+    '''
+    # Find indices of empty bins
+    empty_bins = np.where(
+        np.isnan(DX.Uocean).mean('TIME')*100>(100-thr_perc))[0]
+    # Count
+    Nbins_orig = DX.dims['BINS']
+    Nbins_drop = len(empty_bins)
+
+    # Drop from dataset
+    DX = DX.drop_sel(BINS = empty_bins)
+    # Note in history
+    DX.attrs['history'] += (
+        '\nDropped %i of %i bins where'%(Nbins_drop, Nbins_orig)
+        + ' less than %.1f%% of samples were'%(thr_perc)
+        + '  valid. -> Remaining bins: %i'%(DX.dims['BINS']))
+    return DX
+
+
+def reject_sidelobe(DX):
+    '''
+    Mask samples where we expect sidelobe interference.
+
+    From Nortek documentation
+    (nortekgroup.com/assets/software/N3015-011-SignaturePrinciples.pdf):
+    Maximum range Rmax is given by: 
+
+    (1)    Rmax = A cos(θ) - s_c
+
+    Where A is the distance between transducer and surface, θ the beam 
+    angle (assumed to be 25 deg for Signatures), and s_c the velocity cell 
+    size.
+
+    We calculate A as:
+
+    (2)    A = DEP - ICE_DRAFT
+
+    Where DEPTH is sample-mean depth and ICE_DRAFT is sample-mean
+    sea ice draft (SEA_ICE_DRAFT_MEDIAN_LE if available, otherwise 0).
+
+    In (2), we use sample mean tilt for θ.
+
+    '''
+
+    if 'depth' in DX.keys():
+        DEP = DX.depth.mean(dim='SAMPLE')
+    else:
+        raise Exception('No "depth" field present. -> cannot reject '
+        'measurements influenced by sidelobe interference. ' 
+        'Run sig_calc.dep_from_p() first.')
+    
+    if 'SEA_ICE_DRAFT_LE' in DX.keys():
+        ICE_DRAFT = DX.SEA_ICE_DRAFT_MEDIAN_LE.copy().fillna(0)
+
+    else:
+        ICE_DRAFT = 0
+    
+    A = DEP - ICE_DRAFT
+    cos_theta = np.cos(25*np.pi/180.0)
+    s_c = DX.cell_size_oceanvel
+
+    Rmax = A * cos_theta - s_c
+
+    # Make a copy with only velocities
+    DX_uv = DX[['uocean', 'vocean']]
+    # NaN instances where bin depth is less than 
+    #   DEP-Rmax
+    DX_uv = DX_uv.where(DX.bin_depth > (DEP-Rmax).expand_dims(
+                                dim = {'BINS':DX.dims['BINS']}))
+    
+    # Feed the NaNed (uocean, vocean) fields back into DX
+     
+    N_before = np.sum(~np.isnan(DX.uocean)) # Count samples
+    for key in ['uocean', 'vocean']:
+        DX[key] = DX_uv[key] 
+    N_after = np.sum(~np.isnan(DX.uocean)) # Count samples
+
+    # + Add processing history   
+    proc_string = ('\nRejected samples close enough to the surface '
+        'to be affected by sidelobe interference (rejecting '
+        '%.2f%% of velocity samples).'%((1-N_after/N_before)*100))
+ 
+    for key in ['uocean', 'vocean']:
+        DX[key].attrs['processing_history'] += proc_string
+
+    # Recompute sample averages
+    DX = _calculate_uvocean_avg(DX)
+    return DX
+
+
+def interp_oceanvel(DX, ip_depth):
+    '''
+    Interpolate Uocean, Vocean onto a fixed depth *ip_depth*
+    '''
+
+    U_IP = DX.Uocean.mean('BINS', keep_attrs = True).copy()#*np.nan
+    V_IP = DX.Vocean.mean('BINS', keep_attrs = True).copy()#*np.nan
+    U_IP[:] = np.nan
+    V_IP[:] = np.nan
+
+    for nn in np.arange(DX.dims['TIME']):
+        ip_ = interp1d(DX.bin_depth.isel(TIME=nn),
+                    DX.Uocean.isel(TIME=nn),bounds_error=False,
+                    fill_value=np.nan)
+        U_IP[nn] = ip_(ip_depth)
+        
+        if nn/10 == nn//10:
+            print('Interpolating "Uocean" (%.1f%%)...\r'%(
+                100*nn/DX.dims['TIME']), end = '')
+    print('Interpolating "Uocean": *DONE*     \r', end = '')
+
+    for nn in np.arange(DX.dims['TIME']):
+        ip_ = interp1d(DX.bin_depth.isel(TIME=nn),
+                    DX.Vocean.isel(TIME=nn),bounds_error=False,
+                    fill_value=np.nan)
+        V_IP[nn] = ip_(ip_depth)
+        
+        if nn/10 == nn//10:
+            print('Interpolating "Vocean" (%.1f%%)...\r'%(
+                100*nn/DX.dims['TIME']), end = '')
+    print('Interpolating "Vocean": *DONE*     \r', end = '')
+
+    V_IP_name = 'Vocean_%im'%(np.round(ip_depth))
+    DX[V_IP_name] = V_IP
+    DX[V_IP_name].attrs['long_name'] = (DX.Vocean.attrs['long_name'] 
+                            + ' interpolated to %.1f m depth'%ip_depth)
+    DX[V_IP_name].attrs['processing_history'] = (
+        DX.Vocean.attrs['processing_history'] 
+        + '\nInterpolated to %.1f m depth.'%ip_depth)
+
+    U_IP_name = 'Uocean_%im'%(np.round(ip_depth))
+    DX[U_IP_name] = U_IP
+    DX[U_IP_name].attrs['long_name'] = (DX.Uocean.attrs['long_name'] 
+                            + ' interpolated to %.1f m depth'%ip_depth)
+    DX[U_IP_name].attrs['processing_history'] = (
+        DX.Uocean.attrs['processing_history'] 
+        + '\nInterpolated to %.1f m depth.'%ip_depth)
+
+    print('Added interpolated velocities: (%s, %s)'%(U_IP_name, V_IP_name))
+    return DX
+
+    
